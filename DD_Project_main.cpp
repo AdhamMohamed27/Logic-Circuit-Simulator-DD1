82a9199de84b5c677166b6e2969668f86da0df0f
Hoda-Magdy <125324753+Hoda-Magdy@users.noreply.github.com>
2024-03-07 16:54:46 +0200

    read from files

--- DD_Project_main.cpp
+++ DD_Project_main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include<vector>
 using namespace std;
 
 int main(){
-    ifstream myfile ("filenam.txt");
+    string fPath, outputFilePath;
+    cout << "Enter the circuits' file : ";
+    getline(cin, fPath);
+    cout << "Enter output file name and path: ";
+    getline(cin, outputFilePath);
+
+    // Reading contents of file into vectors
+    vector<vector<string>> lists;
+    ifstream file;
+    string fileName;
+  
+        file.open(fileName);
+        if (!file.is_open()) {
+            cerr << "Error opening file: " << fileName << endl;
+            continue;
+        
+
+        vector<string> s;
+       string x;
+        while (file >> x) {
+            s.push_back(x);
+        }
+        file.close();
     return 0;
 }
